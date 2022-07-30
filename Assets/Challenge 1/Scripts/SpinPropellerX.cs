5746ffa4ec829cdba1da4e136c6878efbb916cf2
leonadX <achimugu24@gmail.com>
2022-07-30 18:26:08 +0100

    upload

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, Time.deltaTime * turnSpeed);
+    }
+}
